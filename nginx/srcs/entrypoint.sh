9ff10dc4355369fb252d66fa9ef3210ebb92020d
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-01-15 21:00:32 +0900

    [fix]wordpress Dockerfile nginx.conf

--- nginx/srcs/entrypoint.sh
+++ nginx/srcs/entrypoint.sh
@@ -3,7 +3,6 @@
 rc-status
 rc-service sshd start
 
-#rc-service nginx start
 nginx -g 'daemon off;'
 
 #tail -f /dev/null
---
+++ wordpress/srcs/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#phpfpmの起動
+#php-fpm7
+#nginxの起動
+#nginx -g 'daemon off;'
+
+tail -f /dev/null
