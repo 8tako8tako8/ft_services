fc34e61681ef8ab9c130786d3b48496a72d3738a
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-02-04 20:00:58 +0900

    add: livenessprobe用にhealthcheck.shを書いてみた。テスト前

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo kubectl delete --all services
+sudo kubectl delete --all deployments
+sudo kubectl delete --all pods
+sudo kubectl delete --all pvc
+sudo kubectl delete --all ingresses
